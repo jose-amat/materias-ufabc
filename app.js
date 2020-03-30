0c19018da3d7976f6ec3f1160a3e0b7e635c6d50
Jose Amat <jose.alc.luis@gmail.com>
2020-03-30 13:26:14 -0300

    nodemon + morgan + tratamento de erro

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
+// END-POINTS
 const rotaMaterias = require('./routes/materias');
 
+app.use(morgan('dev'));
 app.use('/materias', rotaMaterias);
+
+// ROTA NÃO ENCONTRADA
+app.use((req, res, next) => {
+  const erro = new Error('Não encontrado');
+  erro.status = 404;
+  next(erro);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  return res.send({
+    erro: {
+      mensagem: error.message
+    }
+  });
+});
 
 module.exports = app;
--- routes/materias.js
+++ routes/materias.js
@@ -2,38 +2,38 @@ const express = require('express');
 const router = express.Router();
 
 // RETORNA TODAS AS MATERIAS
-router.get('/', (req, rest, next) => {
-  rest.status(200).send({
+router.get('/', (req, res, next) => {
+  res.status(200).send({
     mensagem: 'Retorna todas as matérias'
   });
 });
 
 // INSERE UMA MATERIA
-router.post('/', (req, rest, next) => {
-  rest.status(201).send({
+router.post('/', (req, res, next) => {
+  res.status(201).send({
     mensagem: 'A matéria foi criada'
   });
 });
 
 // RETORNA UMA MATERIA ESPECIFICA
-router.get('/:id_materias', (req, rest, next) => {
+router.get('/:id_materias', (req, res, next) => {
   const id = req.params.id_materias;
-  rest.status(200).send({
+  res.status(200).send({
     mensagem: 'Detalhes da matéria',
     id: id
   });
 });
 
 // ALTERA UMA MATERIA
-router.patch('/', (req, rest, next) => {
-  rest.status(201).send({
+router.patch('/', (req, res, next) => {
+  res.status(201).send({
     mensagem: 'Matéria alterada'
   });
 });
 
 // EXCLUI UMA MATERIA
-router.delete('/', (req, rest, next) => {
-  rest.status(201).send({
+router.delete('/', (req, res, next) => {
+  res.status(201).send({
     mensagem: 'Matéria excluída'
   });
 });
