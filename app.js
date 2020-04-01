70b11b1a85966606b438005f0d4d39148bcfbef0
Jose Amat <jose.alc.luis@gmail.com>
2020-04-01 14:57:17 -0300

    API conectada ao banco MySQL

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 // END-POINTS
 const rotaMaterias = require('./routes/materias');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false})); // apenas dados simples
+app.use(bodyParser.json()); // so aceita formato json no body
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Header', 
+  'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+
+  if (req.method === 'OPTIONS'){
+    res.header('Accesss-Control-Allow-Methods',
+    'PUT, POST, PATCH, DELETE, GET'
+    );
+    return res.status(200).send({});
+  }
+  next();
+});
+
 app.use('/materias', rotaMaterias);
 
 // ROTA NÃO ENCONTRADA
--- routes/materias.js
+++ routes/materias.js
@@ -1,40 +1,100 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('../mysql').pool;
 
 // RETORNA TODAS AS MATERIAS
 router.get('/', (req, res, next) => {
-  res.status(200).send({
-    mensagem: 'Retorna todas as matérias'
+  mysql.getConnection((error, conn) => {
+    if (error) { return res.status(500).send({ error: error }) }
+    conn.query(
+      'SELECT * FROM materias;',
+      (error, resultado, fields) => {
+        if (error) { return res.status(500).send({ error: error}) }
+        return res.status(200).send({ response: resultado })
+      }
+    )
   });
 });
 
 // INSERE UMA MATERIA
 router.post('/', (req, res, next) => {
-  res.status(201).send({
-    mensagem: 'A matéria foi criada'
-  });
+  mysql.getConnection((error, conn) => {
+    if (error) { return res.status(500).send({ error: error }) }
+    conn.query(
+      'INSERT INTO materias (nome, creditos, descricao) VALUES (?, ?, ?)',
+      [req.body.nome, req.body.creditos, req.body.descricao],
+      (error, resultado, field) => {
+        conn.release();
+        if (error) { return res.status(500).send({ error: error }) }
+        res.status(201).send({
+          mensagem: 'A matéria foi criada',
+          id_materias: resultado.insertId
+        });
+      }
+    )
+  })
 });
 
 // RETORNA UMA MATERIA ESPECIFICA
 router.get('/:id_materias', (req, res, next) => {
-  const id = req.params.id_materias;
-  res.status(200).send({
-    mensagem: 'Detalhes da matéria',
-    id: id
+  mysql.getConnection((error, conn) => {
+    if (error) { return res.status(500).send({ error: error }) }
+    conn.query(
+      'SELECT * FROM materias WHERE id_materias = ?;',
+      [req.params.id_materias],
+      (error, resultado, field) => {
+        conn.release();
+        if (error) { return res.status(500).send({ error: error }) }
+        return res.status(201).send({ response: resultado });
+      }
+    )
   });
 });
 
 // ALTERA UMA MATERIA
 router.patch('/', (req, res, next) => {
-  res.status(201).send({
-    mensagem: 'Matéria alterada'
+  mysql.getConnection((error, conn) => {
+    if (error) { return res.status(500).send({ error: error })}
+    conn.query(
+      `UPDATE materias
+        SET nome              = ?,
+            creditos          = ?,
+            descricao         = ?
+      WHERE id_materias       = ?`,
+      [
+        req.body.nome, 
+        req.body.creditos, 
+        req.body.descricao, 
+        req.body.id_materias
+      ],
+      (error, resultado, fields) => {
+        conn.release();
+        if (error) { return res.status(500).send({ error: error }) }
+
+        res.status(202).send({
+          mensagem: 'Materia alterada'
+        });
+      }
+    )
   });
 });
 
 // EXCLUI UMA MATERIA
 router.delete('/', (req, res, next) => {
-  res.status(201).send({
-    mensagem: 'Matéria excluída'
+  mysql.getConnection((error, conn) => {
+    if (error) { return res.status(500).send({ error: error })}
+    conn.query(
+      'DELETE FROM materias WHERE id_materias = ?',
+      [req.body.id_materias],
+      (error, resultado, fields) => {
+        conn.release();
+        if (error) { return res.status(500).send({ error: error }) }
+
+        res.status(202).send({
+          mensagem: 'Materia removido com sucesso'
+        });
+      }
+    )
   });
 });
 
