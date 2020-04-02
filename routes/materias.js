817a8bf9be23d0ff234996bbe823a21c8d0e9797
Jose Amat <jose.alc.luis@gmail.com>
2020-04-02 15:09:29 -0300

    Ações REST documentadas

--- routes/materias.js
+++ routes/materias.js
@@ -2,56 +2,107 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('../mysql').pool;
 
-// RETORNA TODAS AS MATERIAS
+// GET ALL
 router.get('/', (req, res, next) => {
   mysql.getConnection((error, conn) => {
     if (error) { return res.status(500).send({ error: error }) }
     conn.query(
       'SELECT * FROM materias;',
-      (error, resultado, fields) => {
+      (error, result, fields) => {
         if (error) { return res.status(500).send({ error: error}) }
-        return res.status(200).send({ response: resultado })
+        const response = {
+          quantidade: result.length,
+          materias: result.map(mat => {
+            return {
+              id_materias: mat.id_materias,
+              nome: mat.nome,
+              creditos: mat.creditos,
+              descricao: mat.descricao,
+              request: {
+                tipo: 'GET',
+                descricao: 'Retorna uma materia com ID especifico',
+                url: 'http://localhost:3000/materias/' + mat.id_materias
+              }
+            }
+          })
+        }
+
+        return res.status(200).send(response)
       }
     )
   });
 });
 
-// INSERE UMA MATERIA
+// POST
 router.post('/', (req, res, next) => {
   mysql.getConnection((error, conn) => {
     if (error) { return res.status(500).send({ error: error }) }
     conn.query(
       'INSERT INTO materias (nome, creditos, descricao) VALUES (?, ?, ?)',
       [req.body.nome, req.body.creditos, req.body.descricao],
-      (error, resultado, field) => {
+      (error, result, field) => {
         conn.release();
         if (error) { return res.status(500).send({ error: error }) }
-        res.status(201).send({
-          mensagem: 'A matéria foi criada',
-          id_materias: resultado.insertId
-        });
+        const response = {
+          mensagem: 'Materia inserida com sucesso',
+          materiCriada: {
+            id_materias: result.id_materias,
+            nome: req.body.nome,
+            creditos: req.body.creditos,
+            descricao: req.body.descricao,
+            request: {
+              tipo: 'POST',
+              descricao: 'Insere uma materia',
+              url: 'http://localhost:3000/materias'
+            }
+          }
+        }
+
+        res.status(201).send(response);
       }
     )
   })
 });
 
-// RETORNA UMA MATERIA ESPECIFICA
+// GET SINGLE
 router.get('/:id_materias', (req, res, next) => {
   mysql.getConnection((error, conn) => {
     if (error) { return res.status(500).send({ error: error }) }
     conn.query(
       'SELECT * FROM materias WHERE id_materias = ?;',
       [req.params.id_materias],
-      (error, resultado, field) => {
+      (error, result, field) => {
         conn.release();
         if (error) { return res.status(500).send({ error: error }) }
-        return res.status(201).send({ response: resultado });
+        
+        if (result.length == 0){
+          return res.status(404).send({
+            mensagem: 'Nao foi encontrado materia com o ID '+ 
+            req.params.id_materias
+          })
+        }
+
+        const response = {
+          materia: {
+            id_materias: result[0].id_materias,
+            nome: result[0].nome,
+            creditos: result[0].creditos,
+            descricao: result[0].descricao,
+            request: {
+              tipo: 'GET',
+              descricao: 'Retorna todas as materias',
+              url: 'http://localhost:3000/materias/' + result[0].id_materias
+            }
+          }
+        }
+
+        res.status(200).send(response);
       }
     )
   });
 });
 
-// ALTERA UMA MATERIA
+// PACTH
 router.patch('/', (req, res, next) => {
   mysql.getConnection((error, conn) => {
     if (error) { return res.status(500).send({ error: error })}
@@ -67,32 +118,54 @@ router.patch('/', (req, res, next) => {
         req.body.descricao, 
         req.body.id_materias
       ],
-      (error, resultado, fields) => {
+      (error, result, fields) => {
         conn.release();
         if (error) { return res.status(500).send({ error: error }) }
-
-        res.status(202).send({
-          mensagem: 'Materia alterada'
-        });
+        
+        const response = {
+          mensagem: 'Materia atualizada com sucesso',
+          materiaAtualizada:{
+              id_materias: req.body.id_materias,
+              nome: req.body.nome,
+              creditos: req.body.creditos,
+              descricao: req.body.descricao,
+              request: {
+                tipo: 'PACTH',
+                descricao: 'Retorna uma materia com ID especifico',
+                url: 'http://localhost:3000/materias/' + req.body.id_materias
+              }
+        }
+      }
+        return res.status(202).send(response)
       }
     )
   });
 });
 
-// EXCLUI UMA MATERIA
+// DELETE
 router.delete('/', (req, res, next) => {
   mysql.getConnection((error, conn) => {
     if (error) { return res.status(500).send({ error: error })}
     conn.query(
       'DELETE FROM materias WHERE id_materias = ?',
       [req.body.id_materias],
-      (error, resultado, fields) => {
+      (error, result, fields) => {
         conn.release();
         if (error) { return res.status(500).send({ error: error }) }
 
-        res.status(202).send({
-          mensagem: 'Materia removido com sucesso'
-        });
+        const response = {
+          mensagem: 'Materia removida com sucesso',
+          request: {
+            tipo: 'DELETE',
+            descricao: 'Apaga uma matéria',
+            url: 'http://localhost:3000/materias',
+            body: {
+              id_materias: 'Number'
+            }
+          }
+        }
+
+        res.status(202).send(response);
       }
     )
   });
